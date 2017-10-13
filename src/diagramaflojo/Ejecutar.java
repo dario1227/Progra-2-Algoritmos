974e01d0fe242663ce5292ca46329f8514da2e9c
kenne <kenne@LAPTOP-C41V0LJ6>
2017-10-12 18:06:48 -0600

    nada

--- src/diagramaflojo/Ejecutar.java
+++ src/diagramaflojo/Ejecutar.java
@@ -4,17 +4,24 @@ package diagramaflojo;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
+
+import admin.arboles.LeeGrafosAST;
 public class Ejecutar extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
+			LeeGrafosAST.saca();
 			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView("DiagramaFlojo.view1");
 		} catch (PartInitException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return null;
 	}
