ad9b70194a26b96a47d953f108f46a6600530710
kenne <kenne@LAPTOP-C41V0LJ6>
2017-10-17 12:12:52 -0600

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/dario1227/Progra-2-Algoritmos.git

--- src/admin/arboles/ColocaImagenes.java
+++ src/admin/arboles/ColocaImagenes.java
@@ -33,7 +33,7 @@ private static int colocarMetodo(int x, ArrayList<StatementLabel> listaStatement
 		
 		label.labelsactual.setPos(x, y);
 		if(label.listaStatements.isEmpty()) {
-			label.lineas.add(LineFactory.crearLabel("botton", x+75, y, 150, 850));
+			label.lineas.add(LineFactory.crearLabel("botton", x+75, y+80, 10, 150));
 			y+=150;
 		}
 		else {
