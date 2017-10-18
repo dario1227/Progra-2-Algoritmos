1f839bab89cf4b010b5b2422790b8e146f9fd840
kenne <kenne@LAPTOP-C41V0LJ6>
2017-10-17 22:13:15 -0600

    Revisiom

--- src/admin/arboles/ColocaImagenes.java
+++ src/admin/arboles/ColocaImagenes.java
@@ -37,6 +37,7 @@ private static int colocarMetodo(int x, ArrayList<StatementLabel> listaStatement
 			else {
 			label.lineas.add(LineFactory.crearLabel("botton", x+75, y+80, 10, 150));}
 			y+=150;
+			//LOL
 		}
 		else {
 			y= colocarMetodo(x-200,label.listaStatements,y);
